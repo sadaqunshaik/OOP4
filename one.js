df26148421aca13421d6972c4c80129593c8ace2
Your Name <sadaqunps@gmail.com>
2024-06-21 22:50:17 +0530

    oops4

---
+++ one.js
@@ -0,0 +1,14 @@
+
+class Account{
+    constructor(){
+        console.log("Account class -Constructor")
+    }
+}
+class SA extends Account{
+    constructor() {
+       // super()
+        console.log("SA class - constructor")
+    }
+}
+let sa1=new SA()
+console.log(sa1)
